4eb818745d74fe529b680b2c8625bcbefb397616
Xiangyu John Wu <john@careerharbour.co.uk>
2016-04-28 10:03:47 +0200

    Successful apps.html dev mode build
    Did you use Linter?
    No
    
    1. What exactly did you do, explain step by step using an ordered list?
    
    a.
    
    2. Why did you do it? Any comments?

--- eulerapps/settings.py
+++ eulerapps/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'problem2',
+    'home',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +57,9 @@ ROOT_URLCONF = 'eulerapps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'serve')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
