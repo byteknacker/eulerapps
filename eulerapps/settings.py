5efa325c1d1c64ed40c9942bb5e9a71c5585c6a1
Xiangyu John Wu <john@careerharbour.co.uk>
2016-05-10 12:19:28 +0200

    Read for testing
    Did you use Linter?
    Yes
    
    1. What exactly did you do, explain step by step using an ordered list?
    
    a. App ready for deployment testing
    
    2. Why did you do it? Any comments?

--- eulerapps/settings.py
+++ eulerapps/settings.py
@@ -118,12 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'serve')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
