ee38ae8c9e399e5a1f2e1508fc351efc3847f2ae
byteknacker <xiangyu.wu.10@gmail.com>
2016-05-09 22:46:38 -0700

    Finished setup for Heroku

--- eulerapps/wsgi.py
+++ eulerapps/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for eulerapps project.
+WSGI config for hackershelp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eulerapps.settings")
-
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
